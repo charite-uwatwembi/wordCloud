d6c039ebd9f6baebfbb1ec3542e87c929aa4cee4
charite-uwatwembi <c.uwatwembi@alustudent.com>
2024-04-21 19:22:59 +0200

    last commit

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-
 const paragraph = document.getElementById("myParagraph");
+const cloud = document.getElementById("myWordCloud")
 const text = paragraph.textContent;
 
 
-const ourText = text.replace(/[,.]/g, "");
-const words = ourText.split(/\s+/);
+function removeCommasAndDots(str) {
+    return str.split('').filter(char => char !== ',' && char !== '.').join('');
+}
 
+const ourText = removeCommasAndDots(text);
+const words = ourText.split(/\s+/);
 
 const result = {};
 
 words.forEach(word => {
+    
     const lowercaseWord = word.toLowerCase();
 
     if (result[lowercaseWord]) {
@@ -17,21 +21,25 @@ words.forEach(word => {
     } else {
         result[lowercaseWord] = 1;
     }
-    
 });
 
+let sorted = Object.keys(result);
+sorted.sort((a, b) => b[1] - a[1]);
 
-let sorted = Object.entries(result)
-    sorted.sort((a, b) => b[1] - a[1]);
+const newValue = sorted.slice(0, 12)
 
-console.log(sorted.slice(0, 12))
-
-
-
+console.log(newValue);
 
 
-
-
+newValue.forEach(word => {
+    const span = document.createElement("span");
+    span.textContent = word
+    
+    cloud.appendChild(span)
+    span.style.cssText = `display: flex`
+    console.log(span)
+   
+})
 
 
 
